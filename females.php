ab103bd40ce5fce5c5900ba8c7780e9821a42473
Brian Matthews <brian@btmatthews.com>
2012-12-13 17:35:37 +0000

    Moved the facebook like code

--- females.php
+++ females.php
@@ -9,15 +9,7 @@
  </head>
  <body>
   <div id="fb-root"></div>
-  <script>
-   (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
-    fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-  </script>
+  <?php include('inc/fb.php'); ?>
   <div id="main">
    <?php include('inc/header.php'); ?>
    <div id="content">
--- males.php
+++ males.php
@@ -8,16 +8,7 @@
   <?php include('inc/cat1.php'); ?>
  </head>
  <body>
-  <div id="fb-root"></div>
-  <script>
-   (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
-    fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-  </script>
+  <?php include('inc/fb.php'); ?>
   <div id="main">
    <?php include('inc/header.php'); ?>
    <div id="content">
