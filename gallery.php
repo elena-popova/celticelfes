6895ad72f179e81914f25091d4b188d699802d0c
Brian Matthews <brian@btmatthews.com>
2012-12-13 19:06:47 +0000

    Converting kittens page to XML & PHP, Fixing styling problems

--- gallery.php
+++ gallery.php
@@ -1,42 +1,42 @@
 <!DOCTYPE html>
 <html>
-	<?php $tab = 4 ?>
-	<head>
-		<?php include('inc/head.php'); ?>
-		<title>Celticelfes Gallery | Pedigree Devon Rex kittens for sale in Ireland</title>
-		<link rel="stylesheet" type="text/css" href="ad-gallery/jquery.ad-gallery.css">
-		<script src="galleria/galleria-1.2.8.min.js"></script>
-		<script type="text/javascript">
-			var data = [
-				<?php
-					$xml = simplexml_load_file('data/gallery.xml');
-					foreach ($xml->xpath('//item') as $item) {
-						echo "\t{ image:'".$item->image."'";
-						if ($item->title) {
-							echo ", title:'".$item->title."'";
-						}
-						if ($item->description) {
-							echo ", description:'".$item->description."'";
-						}
-						if ($item->layer) {
-							echo ", layer:'".$item->layer."'";
-						}
-						echo " },\n";
-					}
-				?>
-			];
-			Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
-			Galleria.run('#galleria', { dataSource: data, lightbox: true, showInfo: true });
-		</script>
-	</head>
-	<body>
-		<?php include('inc/fb.php'); ?>
-		<div id="main">
-			<?php include('inc/header.php'); ?>
-			<div id="content">
-				<div id="galleria"></div>
-			</div>
-			<?php include('inc/footer.php'); ?>
-		</div>
-	</body>
+ <?php $tab = 4 ?>
+ <head>
+  <?php include('inc/head.php'); ?>
+  <title>Celticelfes Gallery | Pedigree Devon Rex kittens for sale in Ireland</title>
+  <link rel="stylesheet" type="text/css" href="ad-gallery/jquery.ad-gallery.css">
+  <script src="galleria/galleria-1.2.8.min.js"></script>
+  <script type="text/javascript">
+   var data = [
+    <?php
+     $xml = simplexml_load_file('data/gallery.xml');
+     foreach ($xml->xpath('//item') as $item) {
+      echo "\t{ image:'".$item->image."'";
+      if ($item->title) {
+       echo ", title:'".$item->title."'";
+      }
+      if ($item->description) {
+       echo ", description:'".$item->description."'";
+      }
+      if ($item->layer) {
+       echo ", layer:'".$item->layer."'";
+      }
+      echo " },\n";
+     }
+    ?>
+   ];
+   Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
+   Galleria.run('#galleria', { dataSource: data, lightbox: true, showInfo: true });
+  </script>
+ </head>
+ <body>
+  <?php include('inc/fb.php'); ?>
+  <div id="main">
+   <?php include('inc/header.php'); ?>
+   <div id="content">
+    <div id="galleria"></div>
+   </div>
+   <?php include('inc/footer.php'); ?>
+  </div>
+ </body>
 </html>
--- kittens.php
+++ kittens.php
@@ -12,7 +12,7 @@
    <div id="content">
 <img src="kittens/top.jpg" style="padding-left:320px"/>
 <p style="text-align: center">
-<a href="mailto:erikalast2006@yahoo.com?Subject=Devon%20Rex">Mail to us about availible kittens</a>
+<a href="mailto:info@celticelfes.com?Subject=Devon%20Rex">Mail to us about availible kittens</a>
 </p>
 <p>
 	<a href="males.html#panek">PL* Agumbamka Domalien (Panek)</a> and 
@@ -20,61 +20,34 @@
 	in 1 week and they will be available for sale and collection after 12 weeks.
 	Keep up to date by following us on <a href="http://www.facebook.com/#!/lastauskiene?fref=ts" target="_parent">Facebook</a> 
 </p>
-<div class="litter">
-<h1>Panek and Nina</h1>	
-<p>Panek and Nina had Flash and Fenix on 21 Jun 2012.</p>
-<div class="gallery">
-<div class="col1">
-	<img src="kittens/panek-nina.jpg"/>
-	<div class="caption">Fenix &amp; <a href="males.html#flash">Flash</a></div>
-</div>
-<div class="col3">
-	<img src="kittens/fenix.jpg"/>
-	<div class="caption">Fenix</div>
-</div>
-<div class="col2">
-	<img src="kittens/flash.jpg"/>
-	<div class="caption"><a href="males.html#flash">Flash</a></div>
-</div>
-</div>
-</div>
-<div class="litter">
-<h1>Panek and Hanna</h1>	
-<p>Panek and Hanna had Eclaire and Eclipse on 7 Jun 2012.</p>
-<div class="gallery">
-<div class="col1">
-	<!--
-	<img src="kittens/panek-hanna.jpg"/>
-	<div class="caption">Eclaire &amp; Eclipse</div>-->
-</div>
-<div class="col3">
-	<img src="kittens/eclaire.jpg"/>
-	<div class="caption"><a href="males.html#eclaire">Eclaire</a></div>
-</div>
-<div class="col2">
-	<img src="kittens/eclips.JPG"/>
-	<div class="caption">Eclipse</div>
-</div>
-</div>
-</div>
-<div class="litter">
-<h1>Panek and Lola</h1>
-<p>Panek and Lola had Disel and Domingo on 3 April 2012</p>
-<div class="gallery">
-<div class="col1">
-    <img src="kittens/Panek-Lola.JPG"/>
-	<div class="caption">Disel and Domingo</div>
-</div>
-<div class="col3">
-<img src="kittens/Domingo.JPG"/>
-	<div class="caption">Domingo</div>
-</div>
-<div class="col2">
-<img src="kittens/Dyzel.JPG"/>
-	<div class="caption">Disel</div>
-</div>
-</div>
-</div>
+    <?php
+     $xml = simplexml_load_file('data/litters.xml');
+     foreach ($xml->xpath('//litter') as $litter) {
+     ?>
+		<div class="litter">
+		<h1><?=$litter->name?></h1>
+		<p><?=$litter->description?></p>	
+		<div class="gallery">
+		<?php if ($litter->pictures->picture[0]->url) { ?>
+		<div class="col1">
+			<img src="<?=$litter->pictures->picture[0]->url?>"/>
+			<div class="caption"><?=$litter->pictures->picture[0]->caption?></div>
+		</div>
+		<?php } ?>
+		<?php if ($litter->pictures->picture[2]->url) { ?>
+		<div class="col3">
+			<img src="<?=$litter->pictures->picture[2]->url?>"/>
+			<div class="caption"><?=$litter->pictures->picture[2]->caption?></div>
+		</div>
+		<?php } ?>
+		<?php if ($litter->pictures->picture[1]->url) { ?>
+		<div class="col2">
+			<img src="<?=$litter->pictures->picture[1]->url?>"/>
+			<div class="caption"><?=$litter->pictures->picture[1]->caption?></div>
+		</div>
+		<?php } ?>
+	</div>
+	<?php } ?>
    </div>
    <?php include('inc/footer.php'); ?>
   </div>
