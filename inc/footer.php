8d82adf80299c144f7492f1fcebb1e4b7c832104
Brian Matthews <brian@btmatthews.com>
2012-12-17 10:26:31 +0000

    Reverted to facebook button

--- inc/footer.php
+++ inc/footer.php
@@ -4,7 +4,7 @@
 			<tr>
 				<td>&copy; 2012 Celticelfes</td>
 				<td><a href="mailto:info@celticelfes.com?Subject=Devon%20Rex">Send mail to us</a></td>
-				<td><div class="fb-like" data-href="http://www.celticelfes.com" data-send="true" data-width="450" data-show-faces="false"></div></td>
+				<td><a href="http://www.facebook.com/#!/lastauskiene?fref=ts" target="_blank"><img border="0" src="images/fb.jpg" alt="FB" /></a></td>
 				<td align="right">Designed by alien_masha</td>
 			</tr>
 		</table>
