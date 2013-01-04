e411afc9c5d563ffbea5056adfe444c0f911d5d2
Brian Matthews <brian@btmatthews.com>
2013-01-04 21:52:55 +0000

    Updated copyright notice to assert copyright for 2013

--- inc/footer.php
+++ inc/footer.php
@@ -2,7 +2,7 @@
 	<div id="footer">
 		<table>
 			<tr>
-				<td>&copy; 2012 Celticelfes</td>
+				<td>&copy; 2012-2013 Celticelfes</td>
 				<td><a href="mailto:info@celticelfes.com?Subject=Devon%20Rex">Send mail to us</a></td>
 				<td><a href="http://www.facebook.com/#!/lastauskiene?fref=ts" target="_blank"><img border="0" src="images/fb.jpg" alt="FB" /></a></td>
 				<td align="right">Designed by alien_masha</td>
