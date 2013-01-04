be629b7f5ff34e92ecff97a1883e279ded88145e
Brian Matthews <brian@btmatthews.com>
2013-01-04 21:26:02 +0000

    Updated with new arrivals

--- kittens.php
+++ kittens.php
@@ -4,22 +4,66 @@
  <?php $title = 'Celticelfes Kittens' ?>
  <head>
   <?php include('inc/head.php'); ?>
+  <script src="galleria/galleria-1.2.8.min.js"></script>
+  <script type="text/javascript">
+   Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
+  </script>
  </head>
  <body>
   <?php include('inc/fb.php'); ?>
   <div id="main">
    <?php include('inc/header.php'); ?>
    <div id="content">
-<img src="kittens/top.jpg" style="padding-left:320px"/>
-<p style="text-align: center">
-<a href="mailto:info@celticelfes.com?Subject=Devon%20Rex">Mail to us about availible kittens</a>
-</p>
+<h1>New Arrivals</h1>
 <p>
 	<a href="males.html#panek">PL* Agumbamka Domalien (Panek)</a> and 
-	<a href="females.html#lola">Rogus Maggie-May (Lola)</a> are expecting babies
-	in 1 week and they will be available for sale and collection after 12 weeks.
-	Keep up to date by following us on <a href="http://www.facebook.com/#!/lastauskiene?fref=ts" target="_parent">Facebook</a> 
+	<a href="females.html#lola">Rogus Maggie-May (Lola)</a> had kittens on December 22, 2012. 
+	There are four kittens available for sale and collection at 12 weeks.
 </p>
+<img src="kittens/g/litter.jpg" style="padding-left:240px" />
+<p>
+    Contact us by 
+	e-mail at <a href="mailto:info@celticelfes.com?Subject=Devon%20Rex%20-%20Litter%20G">info@celticelfes.com</a> or via
+    <a href="http://www.facebook.com/#!/lastauskiene?fref=ts" target="_parent">Facebook</a>.
+</p>
+<div class="new">
+	<div id="new1">
+		<p class="name">
+			Red Tabby Bicolor<br/>
+		 	Female
+		</p>
+		<p class="status">Available</p>
+		<img src="kittens/g/1as.jpg" />
+		<img src="kittens/g/1bs.jpg" />
+	</div>
+	<div id="new2">
+		<p class="name">
+			Red Tabby Bicolor<br/>
+		 	Male
+		</p>
+		<p class="status">Available</p>
+		<img src="kittens/g/2as.jpg" />
+		<img src="kittens/g/2bs.jpg" />
+	</div>
+	<div id="new3">
+		<p class="name">
+			Red Tabby<br/>
+		 	Male
+		</p>
+		<p class="status">Available</p>
+		<img src="kittens/g/3as.jpg" />
+		<img src="kittens/g/3bs.jpg" />
+	</div>
+	<div id="new4">
+		<p class="name">
+			Cream Point<br/>
+		 	Female
+		</p>
+		<p class="status">Reserved</p>
+		<img src="kittens/g/4as.jpg" />
+		<img src="kittens/g/4bs.jpg" />
+	</div>
+</div>
     <?php
      $xml = simplexml_load_file('data/litters.xml');
      foreach ($xml->xpath('//litter') as $litter) {
@@ -29,21 +73,21 @@
 		<p><?=$litter->description?></p>	
 		<div class="gallery">
 		<?php if ($litter->pictures->picture[0]->url) { ?>
-		<div class="col1">
+		<div class="col">
 			<img src="<?=$litter->pictures->picture[0]->url?>"/>
 			<div class="caption"><?=$litter->pictures->picture[0]->caption?></div>
 		</div>
 		<?php } ?>
-		<?php if ($litter->pictures->picture[2]->url) { ?>
-		<div class="col3">
-			<img src="<?=$litter->pictures->picture[2]->url?>"/>
-			<div class="caption"><?=$litter->pictures->picture[2]->caption?></div>
-		</div>
-		<?php } ?>
 		<?php if ($litter->pictures->picture[1]->url) { ?>
-		<div class="col2">
+		<div class="col">
 			<img src="<?=$litter->pictures->picture[1]->url?>"/>
 			<div class="caption"><?=$litter->pictures->picture[1]->caption?></div>
+		</div>
+		<?php } ?>
+		<?php if ($litter->pictures->picture[2]->url) { ?>
+		<div class="col">
+			<img src="<?=$litter->pictures->picture[2]->url?>"/>
+			<div class="caption"><?=$litter->pictures->picture[2]->caption?></div>
 		</div>
 		<?php } ?>
 	</div>
